6d4003ff14b5a28e3a9c36eb517a8258008a0e6d
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-06-10 17:08:09 -0500

    worked on the index.ts file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
 import cors from 'cors'
 import "dotenv/condig";
 
@@ -7,5 +7,23 @@ const app = express();
 //converts the body of API requests into json 
 //automatically for us so we don't have to handle that ourselves and each of our endpoints.
 app.use(express.json());
-//
+//this parses the URL for the API
 app.use(express.urlencoded({extended:true}))
+//cors will prevent certain requests from certain urls.
+//It's a security thing.
+//we'll configure it to recognize our front end and backend.
+app.use(cors())
+
+//get test
+//NOW WITH ADDED TYPES!
+app.get("/api/test", async(req:Request, res:Response)=>{
+    res.json({message:"hello from express endpoint!"});
+});
+
+//
+app.listen(7000, ()=>{
+    console.log("server running on locahost:7000");
+})
+
+
+
