d2a67ee9dc9413d4e06550020134f626b8f51ba0
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-07-03 15:03:43 -0500

    30 minute mark

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,24 @@
+import {Link} from "react-router-dom";
 
 const Header = () => {
   return (
     // the higher the value, the darker the color
     //then add padding. y-axis padding is 6
     <div className="bg-blue-800 py-6">
-      
+      {/* add a container div for margins */}
+      {/* also use flexbox to push things to the edges of the container */}
+      <div className="container mx-auto flex justify-between">
+        <span className="text-3xl text-white font-bold tracking-tight">
+            <Link to="/">MernHolidays.com</Link>
+
+        </span>
+        <span className="flex space-x-2">
+            <Link to="/sign-in"></Link>
+        </span>
+
+      </div>
     </div>
   )
 }
 
+ 
