2289a91e6f0eec8ec00ed8a9e58b365406769f34
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-07-03 14:52:10 -0500

    setting up front end

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+  return (
+    // the higher the value, the darker the color
+    //then add padding. y-axis padding is 6
+    <div className="bg-blue-800 py-6">
+      
+    </div>
+  )
+}
+
---
+++ frontend/src/layouts/Layout.tsx
@@ -0,0 +1,9 @@
+const Layout = () => {
+    return (
+        // align elements in a column using flexbox
+        // it makes sure the app takes up the whole screen
+        <div className="flex flex-col min-h-screen">
+            <Header/>
+        </div>
+    )
+}
